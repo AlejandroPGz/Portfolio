9dd36179e70b4ce940293c2e4dbafba52b9d20f2
Alejandro <alejandroparadiso02@gmail.com>
2024-03-06 19:45:07 -0400

    changes

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -18,7 +18,7 @@ export const Home = () => {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="/curriculum.pdf"
+                  href="/public/curriculum.pdf"
                   download
                 >
                   Currículum
